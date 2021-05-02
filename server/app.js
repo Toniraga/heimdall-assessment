25bfb37a1148e1e7e7a5800a89c456f94c789109
toniraga <eragatony@gmail.com>
2021-05-02 21:55:49 +0100

    deploy server to heroku

--- server/app.js
+++ server/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
 
 const routes = require('./routes/index.routes');
 
 const app = express();
-const buildPath = path.join(__dirname, '..', 'build');
-app.use(express.static(buildPath));
 
 app.use(cors()); //enable cors
 
