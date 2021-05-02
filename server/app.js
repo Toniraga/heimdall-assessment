134cc9c49bc6ba665be70975d93936aab00d98c9
toniraga <eragatony@gmail.com>
2021-05-02 21:57:22 +0100

    deploy server to heroku

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 
 const routes = require('./routes/index.routes');
@@ -7,9 +8,15 @@ const app = express();
 
 app.use(cors()); //enable cors
 
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/', routes);
+
+app.get('/', (_, res) => res.send('shuu! go away....'));
+
+app.get('*', (_, res) => res.send('Welcome to our server....'));
 
 module.exports = app;
